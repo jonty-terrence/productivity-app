afdf14321459b1b2404d3ea48cc42755522a98f7
Jonty-Buchan <jontyterrence@gmail.com>
2020-10-31 16:19:27 +1300

    Allows user to mark a task as completed

--- client/components/TaskDisplay.jsx
+++ client/components/TaskDisplay.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { removeTask } from '../actions'
+
 let taskCount = 0
 
-function handleClick (task) {
+function handleClick (task, dispatch) {
   console.log(task)
+  dispatch(removeTask(task))
 }
 
 class TaskDisplay extends React.Component {
   render () {
+      console.log(this.props)
     return (
-      this.props.tasks.map(task => {
+      this.props.tasks.tasks.map(task => {
         return (
           <>
-            <button onClick={() => handleClick(task)}>Complete</button>
+            <button onClick={() => handleClick(task, this.props.dispatch)}>Complete</button>
             <p key={taskCount++}>{task}</p>
           </>
         )
